7f8cd7af1768c6ddbd53c294d9c94122dd14baa6
khoado <ddangkhoa75@gmail.com>
2022-11-14 16:43:44 +0700

    14/11

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pyrebase
-import tkinter as tk
+import  tkinter as tk
+#import tkinter as tk
 import PyPDF2
-from PIL import Image, ImageTk
-
+import PIL
+import matplotlib.pyplot as pyplot
+import random
+from datetime import datetime
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
 config = {
     "apiKey": "AIzaSyAYFuYoE2-8V6xxbm_ba0zwVfBCEv25KBQ",
     "authDomain": "arduino-1e690.firebaseapp.com",
@@ -14,6 +19,8 @@ config = {
     "measurementId": "G-S18583XP4C"
 }
 
+# def millitime()
+#    return round(time.time()*1000)
 
 def retrivedata():
 
@@ -39,22 +46,66 @@ if __name__ == "__main__":
     label = tk.Label(root, text="Cường Độ Âm Thanh Đang Là:" + str(retrivedata().val()), font=("Arial", 30))
     label.grid(column=0, row=0)
 
-    t = tk.Text(root, height=1, width=20, font=("Arial", 30))
-    t.grid(column=1, row=1)
+    # t = tk.Text(root, height=1, width=20, font=("Arial", 30))
+    # t.grid(column=1, row=1)
 
-    label2 = tk.Label(root, text="Cường Độ Gửi Đến FireBase:", font=("Arial", 30))
-    label2.grid(column=0, row=1)
+    # label2 = tk.Label(root, text="Cường Độ Gửi Đến FireBase:", font=("Arial", 30))
+    # label2.grid(column=0, row=1)
 
     b=tk.Button(root,text="Cập Nhật Dữ Liệu",command=lambda: senddata(t.get("1.0","end-1c")),font=("Arial",30))
 
     b.grid(column=0,row=2,columnspan=2)
-
+    y=0
+    x_values = []
+    y_values = []
+    figure = pyplot.figure()
     def update():
+
+
+        x = random.randint(0, 10);
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S.%f")[:-3]
+        x_values.append(x)
+        y_values.append(current_time)
+        global  y
+        y += 1
+        print(y)
+
+        figure.add_subplot().plot(y_values, x_values)
+        chart = FigureCanvasTkAgg(figure, root)
+        chart.get_tk_widget().grid(row=1, column=0)
+        pyplot.grid()
+        pyplot.xlim(y-5,y)
+        pyplot.ylim(0,10)
         print(retrivedata().val())
         label.config(text="Cường Độ Âm Thanh Đang Là:" + str(retrivedata().val()))
-        root.after(1000, update)
+        root.after(500, update)
+    # y=0
+    # x_values = [0]
+    # y_values = [0]
+    # x=0
+    # figure=pyplot.figure()
+
+    # while(y<100):
+    #     x=random.randint(0 , 10)
+    #     x_values.append(x)
+    #     y_values.append(y)
+    #     y+=1
+    #     figure.add_subplot().plot(y_values,x_values)
+    #     pyplot.xlim(y-10,y)
+    #
+    #     pyplot.pause(0.01)
+    #
+    # pyplot.show()
+
 
 
     update()
 
+
+
+
     root.mainloop()
+
+
+
